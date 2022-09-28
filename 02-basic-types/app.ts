b7fc7c96c3e327fe96abe9b3fcdbefbb1c0af551
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-09-28 07:25:44 +0200

    array interference

--- 02-basic-types/app.ts
+++ 02-basic-types/app.ts
@@ -2,6 +2,15 @@
 //     name: string, age: number,
 // } =
 const person =  // type interference
-    {name: 'Chris', age: 42};
+    {
+        name: 'Chris', age: 42,
+        hobbies: ['Sports', 'Cooking']
+    };
 
-console.log(person);
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+    // hobby.map() would be an error. map is no method on string :)
+}
