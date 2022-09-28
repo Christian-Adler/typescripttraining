ccd239be43d64b43866fcffc4f8aff3d4db0b63e
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-09-28 07:36:37 +0200

    added custom type enum

--- 02-basic-types/app.ts
+++ 02-basic-types/app.ts
@@ -6,20 +6,30 @@
 //         name: 'Chris', age: 42,
 //         hobbies: ['Sports', 'Cooking'],
 //     };
-const person: {
-    name: string, age: number, hobbies: string[],
-    role: [number, string]  // tuple (special array with strict length and types
-} =
+
+// const person: {
+//     name: string, age: number, hobbies: string[],
+//     role: [number, string]  // tuple (special array with strict length and types
+// } =
+//     {
+//         name: 'Chris', age: 42,
+//         hobbies: ['Sports', 'Cooking'],
+//         role: [2, 'author'] // explicit
+//     };
+
+//custom type by enum
+enum Role {ADMIN = 5, READ_ONLY = 11, AUTHOR}
+
+const person =
     {
         name: 'Chris', age: 42,
         hobbies: ['Sports', 'Cooking'],
-        role: [2, 'author'] // explicit
+        role: Role.AUTHOR
     };
-
 
 console.log(person.name);
 
-person.role = [0, 'admin'];
+person.role = Role.ADMIN;
 
 for (const hobby of person.hobbies) {
     console.log(hobby.toUpperCase());
