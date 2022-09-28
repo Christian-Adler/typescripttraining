4bb3fda62b6422061c8508f2c3ea92d66f90ec5f
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-09-28 07:30:08 +0200

    added tuple

--- 02-basic-types/app.ts
+++ 02-basic-types/app.ts
@@ -1,14 +1,25 @@
 // const person: {
 //     name: string, age: number,
 // } =
-const person =  // type interference
+// const person =  // type interference
+//     {
+//         name: 'Chris', age: 42,
+//         hobbies: ['Sports', 'Cooking'],
+//     };
+const person: {
+    name: string, age: number, hobbies: string[],
+    role: [number, string]  // tuple (special array with strict length and types
+} =
     {
         name: 'Chris', age: 42,
-        hobbies: ['Sports', 'Cooking']
+        hobbies: ['Sports', 'Cooking'],
+        role: [2, 'author'] // explicit
     };
 
 
 console.log(person.name);
+
+person.role = [0, 'admin'];
 
 for (const hobby of person.hobbies) {
     console.log(hobby.toUpperCase());
