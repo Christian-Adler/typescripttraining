9629b46264c5f1c3ed56a1a4494b3489f5ffb9d0
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-10-01 17:44:43 +0200

    enabled ide debugging

--- 03-compiler-config/src/app.ts
+++ 03-compiler-config/src/app.ts
@@ -1,1 +1,9 @@
-console.log('run tsc -w to watch all files...');
+console.log('run tsc -w to watch all files...!');
+
+const btn = document.getElementsByTagName('button')[0];
+
+const clickHandler = () => {
+    console.log('clicked');
+}
+
+btn.addEventListener('click', clickHandler);
