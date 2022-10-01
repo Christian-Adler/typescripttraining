4239554ae1eea3b0137ebecb172ab10388a182cc
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-10-01 07:13:10 +0200

    literal type (special version of union type)

--- 02-basic-types/app.ts
+++ 02-basic-types/app.ts
@@ -1,14 +1,27 @@
-const combine = (input1: number | string, input2: number | string) => {
+/**
+ *
+ * @param input1
+ * @param input2
+ * @param resultConversion literal type
+ */
+const combine = (input1: number | string, input2: number | string, resultConversion: 'as-number' | 'as-text') => {
     let result;
     if (typeof input1 === 'number' && typeof input2 === 'number')
         result = input1 + input2;
     else
         result = input1.toString() + input2.toString();
-    return result;
+
+    if (resultConversion === 'as-number')
+        return +result; // = parseFloat
+
+    return result.toString();
 }
 
-const combinedAges = combine(30, 26);
+const combinedAges = combine(30, 26, 'as-number');
 console.log(combinedAges);
 
-const combinedNames = combine('A', 'B');
+const combinedStringAges = combine('30', '26', 'as-number');
+console.log(combinedStringAges);
+
+const combinedNames = combine('A', 'B', 'as-text');
 console.log(combinedNames);
