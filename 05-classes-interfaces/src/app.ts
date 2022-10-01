5d699570a72c71c2576d33d54a214a8912d631b1
Chris <chris.adler@gmx.de>
2022-10-01 21:40:14 +0200

    added method with this parameter

--- 05-classes-interfaces/src/app.ts
+++ 05-classes-interfaces/src/app.ts
@@ -4,8 +4,18 @@ class Department {
     constructor(n: string) {
         this.name = n;
     }
+
+    describe(this: Department) {
+        console.log('Department: ' + this.name);
+    }
 }
 
 const accounting = new Department('Accounting');
 
 console.log(accounting);
+
+accounting.describe();
+
+// const accountingCopy = {describe: accounting.describe};
+// accountingCopy.describe(); // prints undefinded  - because "this" is the accountingCopy Object which has no name property
+// By adding this: Pseudoparameter an error is enforced
