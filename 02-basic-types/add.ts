3817e66a92f38ff9eea99055f3c2001eb6bec7dd
Christian Adler <37087119+Christian-Adler@users.noreply.github.com>
2022-10-01 12:13:45 +0200

    never type

--- 02-basic-types/add.ts
+++ 02-basic-types/add.ts
@@ -7,3 +7,9 @@ userInput = 'Chris';
 // userName = userInput; nicht erlaubt
 if (typeof userInput === 'string') // Unknown has to be checked before. This why unknown is better than any
     userName = userInput;
+
+const generateError = (message: string, code: number): never => {
+    throw {message, errorCode: code}; // break calling script
+}
+
+generateError('err occurred', 500);
