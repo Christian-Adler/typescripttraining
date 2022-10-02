fc89b66945175eae7431ef587aee145b33429f2b
Chris <chris.adler@gmx.de>
2022-10-02 14:41:33 +0200

    introduced intersection types (Object-Types combine - primitive types intersect)

--- 06-advanced-types/src/app.ts
+++ 06-advanced-types/src/app.ts
@@ -1,1 +1,18 @@
-// Code goes here!
+type Admin = {
+    name: string;
+    privileges: string[];
+};
+
+type Employee = { name: string; startDate: Date };
+
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+    name: 'Chris',
+    privileges: ['create-server'],
+    startDate: new Date()
+}
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+type Universal = Combinable & Numeric;
