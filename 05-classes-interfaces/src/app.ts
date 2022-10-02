8a30e8398742963db6788084ece210b1c0e62d07
Chris <chris.adler@gmx.de>
2022-10-02 05:45:41 +0200

    used private field

--- 05-classes-interfaces/src/app.ts
+++ 05-classes-interfaces/src/app.ts
@@ -1,5 +1,6 @@
 class Department {
     name: string;//name:string ='DEFAULT';
+    private employees: string[] = [];
 
     constructor(n: string) {
         this.name = n;
@@ -8,13 +9,31 @@ class Department {
     describe(this: Department) {
         console.log('Department: ' + this.name);
     }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee);
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
 
 const accounting = new Department('Accounting');
 
-console.log(accounting);
+// console.log(accounting);
+
+accounting.addEmployee("Chris");
+accounting.addEmployee("Max2");
+
+// no longer allowed because of private
+// console.log(accounting.employees);
+// accounting.employees.push('Test');
+// accounting.employees[2] = 'Test2';
 
 accounting.describe();
+accounting.printEmployeeInformation();
 
 // const accountingCopy = {describe: accounting.describe};
 // accountingCopy.describe(); // prints undefinded  - because "this" is the accountingCopy Object which has no name property
