6b63c3289a895133811abc6706a91cecf5f5af9b
Chris <chris.adler@gmx.de>
2022-10-02 06:53:30 +0200

    implemented interface

--- 05-classes-interfaces/src/app.ts
+++ 05-classes-interfaces/src/app.ts
@@ -1,16 +1,21 @@
-interface Person {
+interface Greetable {
     name: string;
     age: number;
 
     greet(phrase: string): void;
 }
 
-let user1: Person;
+class Person implements Greetable {
+    constructor(public name: string, public age: number) {
+    }
 
-user1 = {
-    name: 'Chris', age: 41, greet(phrase: string) {
+    greet(phrase: string) {
         console.log(phrase + ' ' + this.name);
     }
 }
+
+let user1: Greetable;
+
+user1 = new Person('Chris', 41);
 
 user1.greet('Hi there - my name is');
