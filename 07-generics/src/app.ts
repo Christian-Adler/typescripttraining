02acb2b7d532138ae51760e6b1b9a81fb8b4f4ba
Chris <chris.adler@gmx.de>
2022-10-02 18:27:42 +0200

    used built in generics

--- 07-generics/src/app.ts
+++ 07-generics/src/app.ts
@@ -4,3 +4,12 @@ const names: Array<string> = [];
 
 console.log(names);
 
+const promise: Promise<string> = new Promise((resolve /*, reject*/) => {
+    setTimeout(() => {
+        resolve('this is done');
+    }, 1000);
+});
+
+promise.then(data => {
+    data.split(' ')
+})
