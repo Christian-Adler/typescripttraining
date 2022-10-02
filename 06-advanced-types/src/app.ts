69ef81253405a16025cfc93be2bfb1f3e0f77b4a
Chris <chris.adler@gmx.de>
2022-10-02 14:52:27 +0200

    added type guards

--- 06-advanced-types/src/app.ts
+++ 06-advanced-types/src/app.ts
@@ -16,3 +16,26 @@ const e1: ElevatedEmployee = {
 type Combinable = string | number;
 type Numeric = number | boolean;
 type Universal = Combinable & Numeric;
+
+const add = (a: Combinable, b: Combinable) => {
+    if (typeof a === 'string' || typeof b === 'string') // Type Guard
+        return a.toString() + b.toString();
+    return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+const printEmployeeInfo = (emp: UnknownEmployee) => {
+    console.log('Name: ' + emp.name);
+
+    if ('privileges' in emp)
+        console.log('Privileges: ' + emp.privileges);
+
+    if ('startDate' in emp)
+        console.log('Start date: ' + emp.startDate);
+}
+
+printEmployeeInfo(e1);
+printEmployeeInfo({name: 'Abc', startDate: new Date()});
+
+
