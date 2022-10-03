7c70b6f426fe709db99f8d918e8c77b419d2ef1c
Chris <chris.adler@gmx.de>
2022-10-03 06:41:31 +0200

    added property decorator

--- 08-decorators/src/app.ts
+++ 08-decorators/src/app.ts
@@ -42,3 +42,35 @@ class Person {
 const pers = new Person();
 
 console.log(pers);
+
+// --
+
+function Log(target: any, propertyName: string | Symbol) {
+    console.log('Property decorator');
+    console.log(target, propertyName);
+}
+
+class Product {
+    @Log
+    private readonly _title: string;
+    private _price: number
+
+    get title() {
+        return this._title;
+    }
+
+    set price(val: number) {
+        if (val > 0)
+            this._price = val;
+        else throw new Error('Invalid price!')
+    }
+
+    constructor(title: string, price: number) {
+        this._title = title;
+        this._price = price;
+    }
+
+    getPriceWithTax(tax: number) {
+        return this._price * (1 + tax);
+    }
+}
