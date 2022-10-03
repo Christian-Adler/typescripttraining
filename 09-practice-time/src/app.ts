b9ab809cb05cac8030b726a6a8e7bbcaa1c1376f
Chris <chris.adler@gmx.de>
2022-10-03 20:37:36 +0200

    added autobind decorator

--- 09-practice-time/src/app.ts
+++ 09-practice-time/src/app.ts
@@ -1,11 +1,23 @@
+function AutoBind(_target: any, _propertyKey: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true, enumerable: false,
+        get() { // Adding an extra layer to the method
+            // this refers to the object we originally defined the method
+            return originalMethod.bind(this);
+        }
+    };
+    return adjDescriptor;
+}
+
 class ProjectInput {
     private templateElement: HTMLTemplateElement;
     private hostElement: HTMLDivElement;
     private readonly element: HTMLFormElement;
 
     private titleInputElement: HTMLInputElement;
-    // private descInputElement: HTMLInputElement;
-    // private peopleInputElement: HTMLInputElement;
+    private descInputElement: HTMLInputElement;
+    private peopleInputElement: HTMLInputElement;
 
     constructor() {
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
@@ -16,21 +28,24 @@ class ProjectInput {
         this.element.id = 'user-input';
 
         this.titleInputElement = this.element.querySelector('#title')! as HTMLInputElement;
-        // this.descInputElement = this.element.querySelector('#description')! as HTMLInputElement;
-        // this.peopleInputElement = this.element.querySelector('#people')! as HTMLInputElement;
+        this.descInputElement = this.element.querySelector('#description')! as HTMLInputElement;
+        this.peopleInputElement = this.element.querySelector('#people')! as HTMLInputElement;
 
         this.configure();
 
         this.attach();
     }
 
+    @AutoBind
     private submitHandler(event: Event) {
         event.preventDefault();
         console.log(this.titleInputElement.value);
+        console.log(this.descInputElement.value);
+        console.log(this.peopleInputElement.value);
     }
 
     private configure() {
-        this.element.addEventListener('submit', this.submitHandler.bind(this));
+        this.element.addEventListener('submit', this.submitHandler);
 
     }
 
