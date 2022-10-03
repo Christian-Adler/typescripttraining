8281645681e7d9160fc06ad3ab9d1da9aa280b5e
Chris <chris.adler@gmx.de>
2022-10-03 19:47:05 +0200

    added submitHandler

--- 09-practice-time/src/app.ts
+++ 09-practice-time/src/app.ts
@@ -1,7 +1,11 @@
 class ProjectInput {
-    templateElement: HTMLTemplateElement;
-    hostElement: HTMLDivElement;
-    element: HTMLFormElement;
+    private templateElement: HTMLTemplateElement;
+    private hostElement: HTMLDivElement;
+    private readonly element: HTMLFormElement;
+
+    private titleInputElement: HTMLInputElement;
+    // private descInputElement: HTMLInputElement;
+    // private peopleInputElement: HTMLInputElement;
 
     constructor() {
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
@@ -9,8 +13,25 @@ class ProjectInput {
 
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.element.id = 'user-input';
+
+        this.titleInputElement = this.element.querySelector('#title')! as HTMLInputElement;
+        // this.descInputElement = this.element.querySelector('#description')! as HTMLInputElement;
+        // this.peopleInputElement = this.element.querySelector('#people')! as HTMLInputElement;
+
+        this.configure();
 
         this.attach();
+    }
+
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        console.log(this.titleInputElement.value);
+    }
+
+    private configure() {
+        this.element.addEventListener('submit', this.submitHandler.bind(this));
+
     }
 
     private attach() {
