f79e7bdb94399ce2df20464806e9fe3d9932ced7
Chris <chris.adler@gmx.de>
2022-10-04 21:28:59 +0200

    added projectlist class

--- 09-practice-time/src/app.ts
+++ 09-practice-time/src/app.ts
@@ -42,6 +42,34 @@ function AutoBind(_target: any, _propertyKey: string, descriptor: PropertyDescri
     return adjDescriptor;
 }
 
+class ProjectList {
+    private templateElement: HTMLTemplateElement;
+    private hostElement: HTMLDivElement;
+    private readonly element: HTMLElement;
+
+    constructor(private type: 'active' | 'finished') {
+        this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement;
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild as HTMLElement;
+        this.element.id = `${this.type}-projects`;
+
+        this.attach();
+        this.renderContent();
+    }
+
+    private renderContent() {
+        const listId = `${this.type}-project-lists`;
+        this.element.querySelector('ul')!.id = listId;
+        this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECTS';
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('beforeend', this.element);
+    }
+}
+
 class ProjectInput {
     private templateElement: HTMLTemplateElement;
     private hostElement: HTMLDivElement;
@@ -121,3 +149,5 @@ class ProjectInput {
 
 const projectInput = new ProjectInput();
 
+const activeProjList = new ProjectList('active');
+const finishedProjList = new ProjectList('finished');
