2e628e0d688a8237e0857290f0c5721dfe2299d6
Chris <chris.adler@gmx.de>
2022-10-06 07:57:30 +0200

    split into files with ts namespaces

---
+++ 10-modules/src/project-model.ts
@@ -0,0 +1,17 @@
+namespace App {
+    export enum ProjectStatus {
+        Active,
+        Finished,
+    }
+
+    export class Project {
+        constructor(
+            public readonly id: string,
+            public readonly title: string,
+            public readonly desc: string,
+            public readonly numOfPeople: number,
+            public status: ProjectStatus
+        ) {
+        }
+    }
+}
