be6cb3be07dd6e77ec7f0aaca0a9e259ea2e1f77
Chris <chris.adler@gmx.de>
2022-10-06 08:52:47 +0200

    11 webpack initial

---
+++ 11-webpack/src/app.ts
@@ -0,0 +1,12 @@
+// Solange der Browser die imports macht, muss .js angehaengt sein!
+import {ProjectInput} from "./components/project-input.js";
+import {ProjectList} from "./components/project-list.js";
+
+/* const projectInput =  */
+new ProjectInput();
+
+/* const activeProjList = */
+new ProjectList("active");
+/* const finishedProjList = */
+new ProjectList("finished");
+
---
+++ 11-webpack/src/decorators/autobind.ts
@@ -0,0 +1,17 @@
+export function AutoBind(
+    _target: any,
+    _propertyKey: string,
+    descriptor: PropertyDescriptor
+) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            // Adding an extra layer to the method
+            // this refers to the object we originally defined the method
+            return originalMethod.bind(this);
+        },
+    };
+    return adjDescriptor;
+}
