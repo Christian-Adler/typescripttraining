e2cfe7296d14f4d745c5e45e71e7f0ff1badc165
Chris <chris.adler@gmx.de>
2022-10-07 09:19:02 +0200

    added Interface and delete funciton

--- 14-ts-with-react/src/App.tsx
+++ 14-ts-with-react/src/App.tsx
@@ -1,19 +1,24 @@
 import React, {useState} from 'react';
 import TodoList from "./components/TodoList";
 import NewTodo from "./components/NewTodo";
+import {Todo} from "./Todo";
 
 const App: React.FC = () => {
-    const [todos, setTodos] = useState<{ id: string, text: string }[]>([])
+    const [todos, setTodos] = useState<Todo[]>([])
 
     const todoAddHandler = (text: string) => {
         // console.log(text);
         setTodos((prevState) => [...prevState, {id: Math.random().toString(), text: text}]);
     };
 
+    const todoDeleteHandler = (id: string) => {
+        setTodos((prevState) => prevState.filter(t => t.id !== id));
+    }
+
     return (
         <div className="App">
             <NewTodo onAddTodo={todoAddHandler}/>
-            <TodoList items={todos}/>
+            <TodoList items={todos} onDeleteTodo={todoDeleteHandler}/>
         </div>
     );
 }
--- 14-ts-with-react/src/components/TodoList.tsx
+++ 14-ts-with-react/src/components/TodoList.tsx
@@ -7,13 +7,16 @@ import React from "react";
 // const TodoList: React.FC<{ items: { id: string, text: string }[] }> = ({items}) => {
 
 interface TodoListProps {
-    items: { id: string, text: string }[]
+    items: { id: string, text: string }[],
+    onDeleteTodo: (id: string) => void,
 }
 
-const TodoList: React.FC<TodoListProps> = ({items}) => {
+const TodoList: React.FC<TodoListProps> = ({items, onDeleteTodo}) => {
     return (<ul>
         {
-            items.map(todo => <li key={todo.id}>{todo.text}</li>)
+            items.map(todo => <li key={todo.id}><span>{todo.text}</span>
+                <button onClick={onDeleteTodo.bind(null, todo.id)}>Del</button>
+            </li>)
         }
     </ul>);
 };
