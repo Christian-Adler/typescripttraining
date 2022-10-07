b4f7a58388608abdebf0012d0f85ae184ab01e6c
Chris <chris.adler@gmx.de>
2022-10-07 05:16:24 +0200

    added class-transformer lib

--- 12-third-party/src/app.ts
+++ 12-third-party/src/app.ts
@@ -1,7 +1,22 @@
+import 'reflect-metadata';
+import {plainToInstance} from "class-transformer";
 import _ from 'lodash';
-
-declare var GLOBAL: string;
+import {Product} from "./model.product";
 
 console.log(_.shuffle([1, 2, 3]));
 
-console.log(GLOBAL);
+// const p1 = new Product('A book', 12.99);
+// console.log(p1.getInformation());
+
+// from Server...
+const products = [{title: 'Title 1', price: 12.34}, {title: 'T2', price: 1.34}];
+// manually transform
+const loadedProducts: Product[] = products.map(p => new Product(p.title, p.price));
+for (const loadedProduct of loadedProducts) {
+    console.log(loadedProduct.getInformation());
+}
+// per lib
+const loadedByTransformer = plainToInstance(Product, products)
+for (const loadedProduct of loadedByTransformer) {
+    console.log(loadedProduct.getInformation());
+}
---
+++ 12-third-party/src/model.product.ts
@@ -0,0 +1,8 @@
+export class Product {
+    constructor(public title: string, public price: number) {
+    }
+
+    getInformation() {
+        return [this.title, `${this.price}`];
+    }
+}
